a9cf10fc2b1eeb0318136fedb0171524806b8cff
adpatil036 <adarsh.patil@sjsu.edu>
2021-10-26 15:12:19 -0700

    minorchaneg

--- form.php
+++ form.php
@@ -121,11 +121,19 @@
             </div>
             <?php
                 $username = "root";
-                $password = "Sjsu@123";
+                $password = "root";
                 $dbname = "defaultdb";
+                
+                $txt1 =" connection Initated";
+                echo "<h2>" . $txt1 . "</h2>";
 
+                
                 $db = new PDO('mysql:host=127.0.0.1;dbname=' . $dbname . ';charset=utf8', $username, $password);
                 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                $txt1 =" connection succefully";
+                echo "<h2>" . $txt1 . "</h2>";
+
 
                 extract($_POST);
                 if(isset($_POST['create'])) {
