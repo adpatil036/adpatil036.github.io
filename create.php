92b9f8d0a07ab84fc661f3eb6a81dc352f82ace3
adpatil036 <adarsh.patil@sjsu.edu>
2021-10-26 14:58:50 -0700

    Minor change

--- create.php
+++ create.php
@@ -1,9 +1,9 @@
 <?php
 $username = "root";
-$password = "Sjsu@123";
+$password = "root";
 $dbname = "defaultdb";
 
-$db = new PDO('mysql:host=127.0.0.1;dbname=' . $dbname . ';charset=utf8', $username, $password);
+$db = new PDO('mysql:host=localhost;dbname=' . $dbname . ';charset=utf8', $username, $password);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 extract($_POST);
