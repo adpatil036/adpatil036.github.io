e32499604bee0724455dc762442f5c219ba3f861
adpatil036 <adarsh.patil@sjsu.edu>
2021-10-26 14:10:29 -0700

    Minor change

--- create.php
+++ create.php
@@ -6,30 +6,30 @@ $dbname = "defaultdb";
 $db = new PDO('mysql:host=127.0.0.1;dbname=' . $dbname . ';charset=utf8', $username, $password);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
-extract($_POST);
-if(isset($_POST)){
-
 $txt1 = "Connection successfully";
 echo "<h2>" . $txt1 . "</h2>";
 
-	$fname 		= $_POST['firstName'];
-	$lname 		= $_POST['lastName'];
-	$email 			= $_POST['email'];
-	$cellphone	    = $_POST['cellphone'];
-	$homephone 		= $_POST['homephone'];
-    $address    = $_POST['address'];
+// extract($_POST);
+// if(isset($_POST)){
 
 
-		$sql = "insert into user (fname,lname,email,cellphone,homephone,address) VALUES (UPPER(?),UPPER(?),UPPER(?),UPPER(?),UPPER(?),UPPER(?))";
-		$stmtinsert = $db->prepare($sql);
-		$result = $stmtinsert->execute([$fname,$lname, $email, $cellphone, $homephone, $address]);
-		if($result){
-			echo '<div style="color:green;"><h3>'.$fname." ".$lname.' : User Successfully saved.</h3></div>';
-		}else{
-			echo 'There were errors while saving the data.';
-		}
-}else{
-	echo 'No data';
-}
+// 	$fname 		= $_POST['firstName'];
+// 	$lname 		= $_POST['lastName'];
+// 	$email 			= $_POST['email'];
+// 	$cellphone	    = $_POST['cellphone'];
+// 	$homephone 		= $_POST['homephone'];
+//     $address    = $_POST['address'];
+
+
+// 		$sql = "insert into user (fname,lname,email,cellphone,homephone,address) VALUES (UPPER(?),UPPER(?),UPPER(?),UPPER(?),UPPER(?),UPPER(?))";
+// 		$stmtinsert = $db->prepare($sql);
+// 		$result = $stmtinsert->execute([$fname,$lname, $email, $cellphone, $homephone, $address]);
+// 		if($result){
+// 			echo '<div style="color:green;"><h3>'.$fname." ".$lname.' : User Successfully saved.</h3></div>';
+// 		}else{
+// 			echo 'There were errors while saving the data.';
+// 		}
+// }else{
+// 	echo 'No data';
+// }
 ?>
